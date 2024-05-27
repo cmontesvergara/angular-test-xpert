42ab2b46af7bca8b391a52fb5fa446b3a1638cb2
Carlos Montes <krlosbergara@gmail.com>
2024-05-26 23:52:39 -0500

    add envs

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = { endpoints:{
+  breeds:'https://api.thecatapi.com/v1/breeds',
+  imageById:'https://api.thecatapi.com/v1/images/{image_id}',
+  images:'https://api.thecatapi.com/v1/images/search?breed_ids={breed_id}',
+}
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment :any = {
+
+  endpoints:{
+    breeds:'https://api.thecatapi.com/v1/breeds',
+    imageById:'https://api.thecatapi.com/v1/images/{image_id}',
+    images:'https://api.thecatapi.com/v1/images/search?breed_ids={breed_id}',
+  }
+
+
+};
